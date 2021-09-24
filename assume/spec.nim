ca5500d27a1eeefc801e686e2861530a034ae67f
Smooth Operator <github@andy.disruptek.com>
2021-09-24 13:05:33 -0400

    add some skipTypes stuff from leorize

--- assume/spec.nim
+++ assume/spec.nim
@@ -5,6 +5,12 @@ type
   AssNode* = distinct NimNode
   NodeLike* = Assnode or NimNode
 
+const
+  Skippable* = {ntyAlias, ntyTypeDesc}
+    ## Type kinds that can be skipped by getTypeSkip
+  SkippableInst* = {ntyTypeDesc}
+    ## Type kinds that can be skipped by getTypeSkipInst
+
 template dot*(a, b: NimNode): NimNode =
   ## for constructing foo.bar
   newDotExpr(a, b)
@@ -102,6 +108,58 @@ proc filter*(f: NodeFilter; n: NimNode): NimNode =
     result = copyNimNode n
     for kid in items(n):
       result.add filter(f, kid)
+
+func applyLineInfo*(n, info: NimNode): NimNode =
+  ## Produce a copy of `n` with line information from `info` applied to it and
+  ## its children.
+  n.filter do (n: NimNode) -> NimNode:
+    result = copyNimNode(n)
+    result.copyLineInfo(info)
+    for c in n.items:
+      result.add c.applyLineInfo(info)
+
+func getTypeSkip*(n: NimNode, skip = Skippable): NimNode =
+  ## Obtain the type of `n`, while skipping through type kinds matching `skip`.
+  ##
+  ## See `Skippable` for supported type kinds.
+  assert skip <= Skippable, "`skip` contains unsupported type kinds: " & $(skip - Skippable)
+  result = getType(n).applyLineInfo(n)
+  if result.typeKind in skip:
+    case result.typeKind
+    of ntyAlias:
+      result = getTypeSkip(result, skip)
+    of ntyTypeDesc:
+      result = getTypeSkip(result[1], skip)
+    else:
+      discard "return as is"
+
+func getTypeInstSkip*(n: NimNode, skip = SkippableInst): NimNode =
+  ## Obtain the type instantiation of `n`, while skipping through type kinds matching `skip`.
+  ##
+  ## See `SkippableInst` for supported type kinds.
+  assert skip <= SkippableInst, "`skip` contains unsupported type kinds: " & $(skip - SkippableInst)
+  result = getTypeInst(n).applyLineInfo(n)
+  if result.typeKind in skip:
+    case result.typeKind
+    of ntyTypeDesc:
+      result = getTypeInstSkip(result[1], skip)
+    else:
+      discard "return as is"
+
+func getTypeImplSkip*(n: NimNode, skip = Skippable): NimNode =
+  ## Obtain the type implementation of `n`, while skipping through type kinds matching `skip`.
+  result = getTypeImpl:
+    getTypeSkip(n, skip)
+  result = result.applyLineInfo(n)
+
+func newTypedesc*(n: NimNode): NimNode =
+  ## Create a typedesc[n]
+  nnkBracketExpr.newTree(bindSym"typedesc", copy(n))
+
+func sameType*(a, b: NimNode): bool =
+  ## A variant of sameType to workaround https://github.com/nim-lang/Nim/issues/18867
+  {.warning: "compiler bug workaround; see https://github.com/nim-lang/Nim/issues/18867".}
+  macros.sameType(a, b) or macros.sameType(b, a)
 
 macro enumValuesAsArray*(e: typed): untyped =
   ## given an enum type, render an array of its symbol fields
