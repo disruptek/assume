26d97dd68eb54d38b6aa97bc382bd2158d638b77
Smooth Operator <github@andy.disruptek.com>
2023-04-12 13:21:22 -0400

    support grinds

--- tests/test.nim
+++ tests/test.nim
@@ -5,66 +5,69 @@ import balls
 
 import assume
 
-suite "assumptions are, like, great":
-  block:
-    ## enumValueAsString on static values
-    type
-      E = enum
-        One = "A"
-        Two = "B"
-    check One.enumValueAsString == "One"
+proc main =
+  suite "assumptions are, like, great":
+    block:
+      ## enumValueAsString on static values
+      type
+        E = enum
+          One = "A"
+          Two = "B"
+      check One.enumValueAsString == "One"
 
-  block:
-    ## enumValueAsString on runtime values
-    type
-      E = enum
-        One = "A"
-        Two = "B"
-    var one = One
-    check one.enumValueAsString == "One"
+    block:
+      ## enumValueAsString on runtime values
+      type
+        E = enum
+          One = "A"
+          Two = "B"
+      var one = One
+      check one.enumValueAsString == "One"
 
-  block:
-    ## do a thing with var sections, let sections, identdefs
-    macro goats(n: typed) =
-      result = newStmtList()
-      for statement in n.items:
-        case statement.kind
-        of nnkVarSection:
-          var i = 0
-          for ident in statement.asIdentDefs:
-            inc i
+    block:
+      ## do a thing with var sections, let sections, identdefs
+      macro goats(n: typed) =
+        result = newStmtList()
+        for statement in n.items:
+          case statement.kind
+          of nnkVarSection:
+            var i = 0
+            for ident in statement.asIdentDefs:
+              inc i
+              result.add:
+                genAst(size = ident.len, tipe = ident[1]):
+                  check size == 3, "identdefs should be len==3"
+                  check tipe isnot void, "identdefs should have a type"
+                  check tipe is int, "in this test, the type is int"
             result.add:
-              genAst(size = ident.len, tipe = ident[1]):
-                check size == 3, "identdefs should be len==3"
-                check tipe isnot void, "identdefs should have a type"
-                check tipe is int, "in this test, the type is int"
-          result.add:
-            genAst(i = newLit i):
-              check i == 1, "expected one identdefs"
-        of nnkLetSection:
-          var i = 0
-          for ident in statement.asIdentDefs:
-            inc i
+              genAst(i = newLit i):
+                check i == 1, "expected one identdefs"
+          of nnkLetSection:
+            var i = 0
+            for ident in statement.asIdentDefs:
+              inc i
+              result.add:
+                genAst(size = ident.len, tipe = ident[1]):
+                  check size == 3, "identdefs should be len==3"
+                  check tipe isnot void, "identdefs should have a type"
+                  check tipe is int, "in this test, the type is int"
             result.add:
-              genAst(size = ident.len, tipe = ident[1]):
-                check size == 3, "identdefs should be len==3"
-                check tipe isnot void, "identdefs should have a type"
-                check tipe is int, "in this test, the type is int"
-          result.add:
-            genAst(i = newLit i):
-              check i == 3, "expected three identdefs"
-          i = 0
-          let mut = toAnVarSection statement
-          for ident in mut.asIdentDefs:
-            inc i
-          result.add:
-            genAst(kind = newLit $mut.kind, i = newLit i):
-              check i == 3, "expected three identdefs"
-              check kind == "nnkVarSection", "lame conversion"
-        else:
-          discard
+              genAst(i = newLit i):
+                check i == 3, "expected three identdefs"
+            i = 0
+            let mut = toAnVarSection statement
+            for ident in mut.asIdentDefs:
+              inc i
+            result.add:
+              genAst(kind = newLit $mut.kind, i = newLit i):
+                check i == 3, "expected three identdefs"
+                check kind == "nnkVarSection", "lame conversion"
+          else:
+            discard
 
-    goats:
-      var n = 4
-      let a, b, c = 3
-      #var (x, y, z) = (5, 6, 7)
+      goats:
+        var n = 4
+        let a, b, c = 3
+        #var (x, y, z) = (5, 6, 7)
+
+main()
--- tests/typeit.nim
+++ tests/typeit.nim
@@ -27,173 +27,176 @@ type
 proc `$`(s: refS): string = "a ref S"
 proc `$`(o: ref O): string = "a ref O"
 
-suite "type iterator":
-  block:
-    ## a simple value
-    type N = int
-    typeIt 3.N, {}:
-      check $it == "3"
+proc main =
+  suite "type iterator":
+    block:
+      ## a simple value
+      type N = int
+      typeIt 3.N, {}:
+        check $it == "3"
 
-  block:
-    ## a more complex value
-    var found: seq[string]
-    typeIt O(n: 2, s: "two"), {}:
-      found.add $it
-    check found == @["2", "two", "a ref S", "a ref O"]
+    block:
+      ## a more complex value
+      var found: seq[string]
+      typeIt O(n: 2, s: "two"), {}:
+        found.add $it
+      check found == @["2", "two", "a ref S", "a ref O"]
 
-  block:
-    ## a more complex value without refs
-    var found: seq[string]
-    typeIt O(n: 2, s: "two"), {titNoRefs}:
-      found.add $it
-    check found == @["2", "two", "a ref S"]
+    block:
+      ## a more complex value without refs
+      var found: seq[string]
+      typeIt O(n: 2, s: "two"), {titNoRefs}:
+        found.add $it
+      check found == @["2", "two", "a ref S"]
 
-  block:
-    ## type alias of an integer
-    type N = int
-    typeIt N, {}:
-      check $it == "N"
+    block:
+      ## type alias of an integer
+      type N = int
+      typeIt N, {}:
+        check $it == "N"
 
-  block:
-    ## bool type; it's gonna be important
-    typeIt bool, {}:
-      check $it == "bool"
+    block:
+      ## bool type; it's gonna be important
+      typeIt bool, {}:
+        check $it == "bool"
 
-  block:
-    ## fields of an object
-    var found: seq[string]
-    typeIt O, {}:
-      found.add $it
-    check found == @["int", "string", "refS", "ref O"]
+    block:
+      ## fields of an object
+      var found: seq[string]
+      typeIt O, {}:
+        found.add $it
+      check found == @["int", "string", "refS", "ref O"]
 
-  block:
-    ## sequence value
-    typeIt @[1, 1, 2, 3, 5], {}:
-      check $it == "@[1, 1, 2, 3, 5]"
+    block:
+      ## sequence value
+      typeIt @[1, 1, 2, 3, 5], {}:
+        check $it == "@[1, 1, 2, 3, 5]"
 
-  block:
-    ## value inheritance
-    var found: seq[string]
-    typeIt I(m: true, y: 3.4, k: false, b: 5.3), {}:
-      found.add $it
-    check found == @["true", "3.4", "false", "5.3"]
+    block:
+      ## value inheritance
+      var found: seq[string]
+      typeIt I(m: true, y: 3.4, k: false, b: 5.3), {}:
+        found.add $it
+      check found == @["true", "3.4", "false", "5.3"]
 
-  block:
-    ## type inheritance
-    var found: seq[string]
-    typeIt I(m: true, y: 3.4, k: false, b: 5.3), {}:
-      found.add $it
-    check found == @["true", "3.4", "false", "5.3"]
+    block:
+      ## type inheritance
+      var found: seq[string]
+      typeIt I(m: true, y: 3.4, k: false, b: 5.3), {}:
+        found.add $it
+      check found == @["true", "3.4", "false", "5.3"]
 
-  block:
-    ## unwrapped distinct values
-    type D = distinct bool
-    proc `$`(d: D): string {.used.} = "d-ish"
-    typeIt true.D, {}:
-      check $it == "true"
+    block:
+      ## unwrapped distinct values
+      type D = distinct bool
+      proc `$`(d: D): string {.used.} = "d-ish"
+      typeIt true.D, {}:
+        check $it == "true"
 
-  block:
-    ## unwrapped distinct types
-    type D = distinct bool
-    proc `$`(d: D): string {.used.} = "d-ish"
-    typeIt D, {}:
-      check $it == "bool"
+    block:
+      ## unwrapped distinct types
+      type D = distinct bool
+      proc `$`(d: D): string {.used.} = "d-ish"
+      typeIt D, {}:
+        check $it == "bool"
 
-  block:
-    ## opaque distinct values
-    type D = distinct bool
-    proc `$`(d: D): string {.used.} = "d-ish"
-    typeIt true.D, {titDistincts}:
-      check $it == "d-ish"
+    block:
+      ## opaque distinct values
+      type D = distinct bool
+      proc `$`(d: D): string {.used.} = "d-ish"
+      typeIt true.D, {titDistincts}:
+        check $it == "d-ish"
 
-  block:
-    ## opaque distinct types
-    type D = distinct bool
-    proc `$`(d: D): string {.used.} = "d-ish"
-    typeIt D, {titDistincts}:
-      check $it == "D"
+    block:
+      ## opaque distinct types
+      type D = distinct bool
+      proc `$`(d: D): string {.used.} = "d-ish"
+      typeIt D, {titDistincts}:
+        check $it == "D"
 
-  block:
-    ## opaque type aliases
-    type N = int
-    typeIt N, {}:
-      check $it == "N"
+    block:
+      ## opaque type aliases
+      type N = int
+      typeIt N, {}:
+        check $it == "N"
 
-  block:
-    ## unwrapped type aliases
-    type N = int
-    typeIt N, {titNoAliases}:
-      check $it == "int"
+    block:
+      ## unwrapped type aliases
+      type N = int
+      typeIt N, {titNoAliases}:
+        check $it == "int"
 
-  block:
-    ## ref object values
-    type R = ref object
-      x: int
-      y: float
-    var found: seq[string]
-    typeIt R(x: 4, y: 1.2), {}:
-      found.add $it
-    check found == @["4", "1.2"]
+    block:
+      ## ref object values
+      type R = ref object
+        x: int
+        y: float
+      var found: seq[string]
+      typeIt R(x: 4, y: 1.2), {}:
+        found.add $it
+      check found == @["4", "1.2"]
 
-  block:
-    ## ref object types
-    type R = ref object
-      x: int
-      y: float
-    var found: seq[string]
-    typeIt R, {}:
-      found.add $it
-    check found == @["int", "float"]
+    block:
+      ## ref object types
+      type R = ref object
+        x: int
+        y: float
+      var found: seq[string]
+      typeIt R, {}:
+        found.add $it
+      check found == @["int", "float"]
 
-  block:
-    ## typeit all fields
-    var 
-      i = I()
-      count = 0
-    typeit(i, {titAllFields}):
-      inc count
-    assert count == 6
+    block:
+      ## typeit all fields
+      var
+        i = I()
+        count = 0
+      typeit(i, {titAllFields}):
+        inc count
+      assert count == 6
 
-  block:
-    ## typeit all fields / isaccessible
-    var
-      p = P()
-      accessible = 0
-      fieldCount = 0
-      res: seq[string]
-    typeit(p, {titAllFields}):
-      if isAccessible(it):
-        inc accessible
-        res.add $it
-      inc fieldCount
-    check res == @["false", "0"]
-    check accessible == 2
-    check fieldCount == 3
+    block:
+      ## typeit all fields / isaccessible
+      var
+        p = P()
+        accessible = 0
+        fieldCount = 0
+        res: seq[string]
+      typeit(p, {titAllFields}):
+        if isAccessible(it):
+          inc accessible
+          res.add $it
+        inc fieldCount
+      check res == @["false", "0"]
+      check accessible == 2
+      check fieldCount == 3
 
-  block:
-    ## isAccessible object test
-    var o = O()
-    check isAccessible(o.n)
-    check isAccessible(o.s)
-    check isAccessible(o.rs)
-    check isAccessible(o.ro)
-  
-  block:
-    ## isAccessible variant test
-    var p = P()
-    check isAccessible(p.m)
-    check isAccessible(p.x)
-    check not isAccessible(p.y)
-    p = P(m: true)
-    check isAccessible(p.m)
-    check not isAccessible(p.x)
-    check isAccessible(p.y)
+    block:
+      ## isAccessible object test
+      var o = O()
+      check isAccessible(o.n)
+      check isAccessible(o.s)
+      check isAccessible(o.rs)
+      check isAccessible(o.ro)
 
-  block:
-    ## isAccessible inheritance variant test
-    var i = I()
-    check isAccessible(i.m)
-    check isAccessible(i.k)
-    check not isAccessible(i.y)
-    check not isAccessible(i.a)
-    check isAccessible(i.b)
+    block:
+      ## isAccessible variant test
+      var p = P()
+      check isAccessible(p.m)
+      check isAccessible(p.x)
+      check not isAccessible(p.y)
+      p = P(m: true)
+      check isAccessible(p.m)
+      check not isAccessible(p.x)
+      check isAccessible(p.y)
+
+    block:
+      ## isAccessible inheritance variant test
+      var i = I()
+      check isAccessible(i.m)
+      check isAccessible(i.k)
+      check not isAccessible(i.y)
+      check not isAccessible(i.a)
+      check isAccessible(i.b)
+
+main()
